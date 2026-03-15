6acf1468fd2b2cf3887d5a6541511a12b0500d13
Andre Faria <andremarcalfaria@gmail.com>
2026-03-15 21:26:22 +0000

    introduce typed log wrappers and preserve existing semantics

--- src/utils/serial_logging.h
+++ src/utils/serial_logging.h
@@ -23,13 +23,46 @@ enum WiimoteLogLevel : uint8_t {
 #define WIIMOTE_LOG_INFO ((uint8_t)WiimoteLogInfo)
 #define WIIMOTE_LOG_DEBUG ((uint8_t)WiimoteLogDebug)
 
+#ifndef WIIMOTE_VERBOSE
+#define WIIMOTE_VERBOSE 2
+#endif
+
 uint8_t wiimoteGetLogLevel();
 void wiimoteSetLogLevel(uint8_t level);
+void wiimoteLogVPrint(uint8_t level, const char *prefix, const char *format, va_list args);
 void wiimoteLogPrint(uint8_t level, const char *prefix, const char *format, ...);
 
-#define LOG_ERROR(...) wiimoteLogPrint(WIIMOTE_LOG_ERROR, "[ERROR] ", __VA_ARGS__)
-#define LOG_WARN(...) wiimoteLogPrint(WIIMOTE_LOG_WARNING, "[WARN] ", __VA_ARGS__)
-#define LOG_INFO(...) wiimoteLogPrint(WIIMOTE_LOG_INFO, "[INFO] ", __VA_ARGS__)
-#define LOG_DEBUG(...) wiimoteLogPrint(WIIMOTE_LOG_DEBUG, "[DEBUG] ", __VA_ARGS__)
+inline void wiimoteLogError(const char *format, ...) {
+    va_list args;
+    va_start(args, format);
+    wiimoteLogVPrint(WIIMOTE_LOG_ERROR, "[ERROR] ", format, args);
+    va_end(args);
+}
+
+inline void wiimoteLogWarn(const char *format, ...) {
+    va_list args;
+    va_start(args, format);
+    wiimoteLogVPrint(WIIMOTE_LOG_WARNING, "[WARN] ", format, args);
+    va_end(args);
+}
+
+inline void wiimoteLogInfo(const char *format, ...) {
+    va_list args;
+    va_start(args, format);
+    wiimoteLogVPrint(WIIMOTE_LOG_INFO, "[INFO] ", format, args);
+    va_end(args);
+}
+
+inline void wiimoteLogDebug(const char *format, ...) {
+    va_list args;
+    va_start(args, format);
+    wiimoteLogVPrint(WIIMOTE_LOG_DEBUG, "[DEBUG] ", format, args);
+    va_end(args);
+}
+
+#define LOG_ERROR(...) wiimoteLogError(__VA_ARGS__)
+#define LOG_WARN(...) wiimoteLogWarn(__VA_ARGS__)
+#define LOG_INFO(...) wiimoteLogInfo(__VA_ARGS__)
+#define LOG_DEBUG(...) wiimoteLogDebug(__VA_ARGS__)
 
 #endif  // ESP32_WIIMOTE_UTILS_SERIAL_LOGGING_H
